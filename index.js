6380eea2975f8aebeea9423fd2d6ab05ee87113b
Jacoblynch99 <jacoblynch99@gmail.com>
2020-09-17 14:16:33 -0500

    every done

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const _ = require("underscore")
 
 const numArr = [25, 36, 68, 1275, 9, 165, 237, 20]
 
-const rejectOdds = (num) => {
+const odds = (num) => {
   return num % 2 != 0
 }
 
-const nums = _.reject(numArr, rejectOdds)
+const everythingPasses = (num) => {
+  return typeof num === "number"
+}
+
+const nums = _.reject(numArr, odds) // this finds and removes all specified elements and return what is left in the array. Essentially this is the opposite of the find function
+
+const bool = _.every(numArr, everythingPasses) // returns true only if every element on an array passes true from the specified function. Goes through the array until it hits an element that does not meet the functions requirements.
 
 console.log(nums)
+console.log(bool)
