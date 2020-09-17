54f372b890729a7827cc2ce8919426af3cbba4fb
Jacoblynch99 <jacoblynch99@gmail.com>
2020-09-17 14:06:48 -0500

    reject done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const variable = require("underscore")
+const _ = require("underscore")
+
+const numArr = [25, 36, 68, 1275, 9, 165, 237, 20]
+
+const rejectOdds = (num) => {
+  return num % 2 != 0
+}
+
+const nums = _.reject(numArr, rejectOdds)
+
+console.log(nums)
